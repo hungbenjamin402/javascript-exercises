b685f595d0713ac79f59fd39e30e30294c50d326
Manh Hung (Benjamin) Nguyen <hungbenjamin402@gmail.com>
2024-09-17 18:29:24 -0700

    Finished 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let newString = '';
 
+    for (let i = word.length - 1; i >= 0; i--) {
+        newString = newString.concat(word.charAt(i));
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
